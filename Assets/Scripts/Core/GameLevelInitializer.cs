fd75f05c373777ad8ae99ca375eefbb6fc3491c4
Paul-Vasylenko <maincrafter1307@gmail.com>
2023-03-26 16:55:44 +0300

    Add logic for 2 controls

---
+++ Assets/Scripts/Core/GameLevelInitializer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Player;
+using UnityEngine;
+
+namespace Core
+{
+    public class GameLevelInitializer : MonoBehaviour
+    {
+        [SerializeField] private PlayerEntity _playerEntity;
+        [SerializeField] private GameUIInputView _gameUIInputView;
+
+        private ExternalDevicesInputReader _externalDevicesInputReader;
+        private PlayerBrain _playerBrain;
+
+        private bool _onPause;
+
+        private void Awake()
+        {
+            _externalDevicesInputReader = new ExternalDevicesInputReader();
+            _playerBrain = new PlayerBrain
+                (
+                    _playerEntity, new List<IEntityInputSource>
+                    {
+                        _externalDevicesInputReader,
+                        _gameUIInputView
+                    }
+                );
+        }
+
+        private void Update()
+        {
+            if(_onPause)
+                return;
+            _externalDevicesInputReader.OnUpdate();
+        }
+
+        private void FixedUpdate()
+        {
+            if(_onPause)
+                return;
+            _playerBrain.OnFixedUpdate();
+        }
+    }
+}
---
+++ Assets/Scripts/ExternalDevicesInputReader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Player;
+using UnityEngine;
+
+public class ExternalDevicesInputReader : IEntityInputSource
+{
+    public float HorizontalDirection => Input.GetAxisRaw("Horizontal");
+    public bool Jump { get; private set;  }
+    public void OnUpdate()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump = true;
+        }
+    }
+
+    public void ResetOneTimeActions()
+    {
+        Jump = false;
+    }
+
+}

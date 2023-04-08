2d237c7b7a8801fdabad54cce5b5aad58af9499e
Paul-Vasylenko <maincrafter1307@gmail.com>
2023-04-08 13:39:04 +0300

    Add potion item

---
+++ Assets/Scripts/Items/Core/Potion.cs
@@ -0,0 +1,39 @@
+﻿using Items.Data;
+using StatsSystem;
+
+namespace Items.Core
+{
+    public class Potion : Item
+    {
+        private readonly StatsController _statsController;
+        private StatChangingItemDescriptor _itemDescriptor;
+        private int _amount;
+        public override int Amount => _amount;
+
+        public Potion(ItemDescriptor descriptor, StatsController statsController) : base(descriptor)
+        {
+            _itemDescriptor = descriptor as StatChangingItemDescriptor;
+            _statsController = statsController;
+            _amount = 1;
+        }
+
+        public override void Use()
+        {
+            _amount--;
+            foreach (var stat in _itemDescriptor.Stats)
+                _statsController.ProcessModificator(stat);
+            if (_amount <= 0)
+                Destroy();
+        }
+
+        private void Destroy()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AddToStack(int amount)
+        {
+            _amount += Amount;
+        }
+    }
+}

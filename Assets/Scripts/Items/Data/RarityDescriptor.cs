9057cca19e572af5613fc117dc99ff7c11bb4648
Vladlen <ilusenkovladlen@gmail.com>
2023-04-09 20:48:42 +0300

    Add ItemRarityDescriptorsStorage

--- Assets/Scripts/Items/Data/RarityDescriptor.cs
+++ Assets/Scripts/Items/Data/RarityDescriptor.cs
@@ -1,7 +1,16 @@
-﻿namespace Items.Data
+﻿using System;
+using Items.Enum;
+using Items.Rarity;
+using UnityEngine;
+using Color = System.Drawing.Color;
+
+namespace Items.Data
 {
-    public class RarityDescriptor
+    [Serializable]
+    public class RarityDescriptor : IItemRarityColor
     {
-        
+        [field: SerializeField] public ItemRarity ItemRarity { get; }
+        [field: SerializeField] public Sprite Sprite { get; private set; }
+        [field: SerializeField] public Color Color { get; }
     }
 }

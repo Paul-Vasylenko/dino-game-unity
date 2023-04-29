6ee287857749e9b52304a5bfd8020526601b06b1
Vlad Zusko <vladzvu11@gmail.com>
2023-04-29 18:24:36 +0300

    GP-23 feat: add inventory screen view

---
+++ Assets/Scripts/UI/InventoryUI/InventoryScreenView.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UI.Core;
+using UI.InventoryUI.Element;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+namespace UI.InventoryUI
+{
+    public class InventoryScreenView : ScreenView
+    {
+        [SerializeField] private Button _closeButton;
+        [SerializeField] private TMP_Text _potionsText;
+
+        [SerializeField] private Transform _backPackContainer;
+        [SerializeField] private Transform _equipmentContainer;
+
+        public List<ItemSlot> ItemSlots { get; private set; }
+        public List<EquipmentSlot> EquipmentSlots { get; private set; }
+
+
+        [field: SerializeField] public Image MovingImage { get; private set; }
+
+        public event Action CloseClicked;
+
+        private void Awake()
+        {
+            _closeButton.onClick.AddListener(() => CloseClicked?.Invoke());
+            ItemSlots = GetComponentsInChildren<ItemSlot>().ToList();
+            EquipmentSlots = GetComponentsInChildren<EquipmentSlot>().ToList();
+        }
+
+        private void OnDestroy()
+        {
+            _closeButton.onClick.RemoveAllListeners();
+        }
+
+        public void SetPotionsAmount(string amount) => _potionsText.text = amount;
+    }
+}

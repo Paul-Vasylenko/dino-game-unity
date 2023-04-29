3086a520086500bf884ec0dc455626d5f8ac6623
Vlad Zusko <vladzvu11@gmail.com>
2023-04-29 18:04:52 +0300

    GP-23 feat: add equipment slot

---
+++ Assets/Scripts/UI/InventoryUI/Element/EquipmentSlot.cs
@@ -0,0 +1,16 @@
+﻿using Items.Enum;
+using UnityEngine;
+
+namespace UI.InventoryUI.Element
+{
+    public class EquipmentSlot : ItemSlot
+    {
+        [field: SerializeField] public EquipmentType EquipmentType { get; private set; }
+
+        public void SetAfterImage(Sprite sprite, Sprite backSprite)
+        {
+            SetItem(sprite, backSprite, -1);
+            ClearButton.gameObject.SetActive(false);
+        }
+    }
+}

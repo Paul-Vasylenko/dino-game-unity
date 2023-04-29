59496efa2ee718ccd2f0f5d22ab9c2be43f208d9
Vlad Zusko <vladzvu11@gmail.com>
2023-04-29 18:29:00 +0300

    GP-23 feat: update inventory logic

--- Assets/Scripts/Items/Inventory.cs
+++ Assets/Scripts/Items/Inventory.cs
@@ -8,38 +8,31 @@ namespace Items
 {
     public class Inventory
     {
-        public int InventorySize = 32;
+        public const int InventorySize = 32;
+        private readonly EquipmentConditionChecker _equipmentFitter;
         private readonly Transform _player;
-        public List<Item> BackpackItems { get; }
+
+        public List<Item> BackPackItems { get; }
         public List<Equipment> Equipment { get; }
+
         public event Action<Item, Vector2> ItemDropped;
         public event Action BackpackChanged;
         public event Action EquipmentChanged;
 
-        public Inventory(List<Item> backpackItems, List<Equipment> equipment, Transform player)
+        public Inventory(List<Item> items, List<Equipment> equipment, Transform player, EquipmentConditionChecker equipmentFitter)
         {
+            _equipmentFitter = equipmentFitter;
+            BackPackItems = new List<Item>();
+            for (var i = 0; i < InventorySize; i++)
+                BackPackItems.Add(null);
+
+            Equipment = new List<Equipment>();
             _player = player;
-            Equipment = equipment ?? new List<Equipment>();
-            if (backpackItems != null) return;
-            
-            BackpackItems = new List<Item>();
-            for (var i = 0; i < InventorySize; i++) 
-                BackpackItems.Add(null);
         }
 
-        public bool TryAddToBackPack(Item item)
-        {
-            if (BackpackItems.All(slot => slot != null))
-                return false;
-
-            var index = BackpackItems.IndexOf(null);
-            PlaceToBackPack(item, index);
-            return true;
-        }
-        
         public bool TryAddToInventory(Item item)
         {
-            if(item is Equipment equipment && 
+            if (item is Equipment equipment &&
                Equipment.All(equip => equip.EquipmentType != equipment.EquipmentType)
                && TryEquip(equipment))
                 return true;
@@ -51,26 +44,44 @@ namespace Items
         {
             if (!(item is Equipment equipment))
                 return false;
-            var oldEquipment = Equipment.Find(slot => slot.EquipmentType == equipment.EquipmentType);
 
-            if (oldEquipment == null)
+            if (!_equipmentFitter.IsEquipmentConditionFits(equipment, Equipment))
+                return false;
+
+            #region InventoryScreen
+            if (!_equipmentFitter.TryReplaceEquipment(equipment, Equipment, out var oldEquipment))
+                return false;
+
+            if (oldEquipment != null)
+                UnEquip(oldEquipment);
+
+            if (BackPackItems.Contains(equipment))
             {
-                Equipment.Add(equipment);
-                equipment.Use();
-                EquipmentChanged?.Invoke();
-                return true;
-            }
-            
-            if (BackpackItems.Contains(equipment))
-            {
-                var indexOfItem = BackpackItems.IndexOf(equipment);
+                var indexOfItem = BackPackItems.IndexOf(equipment);
                 PlaceToBackPack(oldEquipment, indexOfItem);
             }
             else TryAddToBackPack(oldEquipment);
+            #endregion
 
-            return false;
+            Equipment.Add(equipment);
+            equipment.Use();
+            EquipmentChanged?.Invoke();
+            return true;
         }
-        
+
+        private bool TryAddToBackPack(Item item)
+        {
+            if (BackPackItems.All(slot => slot != null))
+                return false;
+
+            var index = BackPackItems.IndexOf(null);
+            PlaceToBackPack(item, index);
+            return true;
+        }
+
+        #region Model
+
+
         public void UseItem(Item item)
         {
             if (item is Potion potion)
@@ -81,33 +92,53 @@ namespace Items
                 return;
             }
 
-            if (item is not Equipment equipment) 
+            if (item is not Equipment equipment)
                 return;
 
             if (Equipment.Contains(equipment))
             {
-                if(TryAddToBackPack(equipment))
+                if (TryAddToBackPack(equipment))
                     UnEquip(equipment);
-                
+
                 return;
             }
 
-            if (!TryEquip(equipment)) 
+            if (!TryEquip(equipment))
                 return;
-            
-            BackpackItems.Remove(item);
+
+            BackPackItems.Remove(item);
             BackpackChanged?.Invoke();
         }
-        
+
         public void RemoveItem(Item item, bool toWorld)
         {
             if (item is Equipment equipment && Equipment.Contains(equipment))
                 UnEquip(equipment);
-            else 
+            else
                 RemoveFromBackBack(item);
-            
-            if(toWorld)
+
+            if (toWorld)
                 ItemDropped?.Invoke(item, _player.position);
+        }
+
+        public void MoveItemToPositionInBackPack(Item item, int place)
+        {
+            if (item is Equipment equipment)
+            {
+                var backPackItem = BackPackItems[place];
+                if (backPackItem != null)
+                {
+                    TryEquip(backPackItem);
+                    return;
+                }
+
+                if (TryPlaceToBackPack(item, place))
+                    UnEquip(equipment);
+
+                return;
+            }
+
+            TryPlaceToBackPack(item, place);
         }
 
         private void UnEquip(Equipment equipment)
@@ -117,68 +148,35 @@ namespace Items
             EquipmentChanged?.Invoke();
         }
 
-        public void RemoveFromBackpack(Item item, bool toWorld)
-        {
-            var index = BackpackItems.IndexOf(item);
-            BackpackItems[index] = null;
-            BackpackChanged?.Invoke();
-            
-            if(toWorld) ItemDropped?.Invoke(item, _player.position);
-
-        }
-
-        public int GetBackpackSize()
-        {
-            return BackpackItems.FindAll(item => item != null).Count;
-        }
-        
-        public void MoveItemToPositionInBackPack(Item item, int place)
-        {
-            if (item is Equipment equipment)
-            {
-                var backPackItem = BackpackItems[place];
-                if (backPackItem != null)
-                {
-                    TryEquip(backPackItem);
-                    return;
-                }
-                
-                if(TryPlaceToBackPack(item, place))
-                    UnEquip(equipment);
-                
-                return;
-            }
-
-            TryPlaceToBackPack(item, place);
-        }
-        
-        private void PlaceToBackPack(Item item, int index)
-        {
-            BackpackItems[index] = item;
-            BackpackChanged?.Invoke();
-        }
-        
         private bool TryPlaceToBackPack(Item item, int index)
         {
-            var oldItem = BackpackItems[index];
-            if (BackpackItems.Contains(item))
+            var oldItem = BackPackItems[index];
+            if (BackPackItems.Contains(item))
             {
-                var indexOfItem = BackpackItems.IndexOf(item);
-                BackpackItems[indexOfItem] = oldItem;
+                var indexOfItem = BackPackItems.IndexOf(item);
+                BackPackItems[indexOfItem] = oldItem;
             }
             else if (oldItem != null)
                 return false;
 
-            BackpackItems[index] = item;
+            BackPackItems[index] = item;
             BackpackChanged?.Invoke();
             return true;
         }
-        
+
+        private void PlaceToBackPack(Item item, int index)
+        {
+            BackPackItems[index] = item;
+            BackpackChanged?.Invoke();
+        }
+
         private void RemoveFromBackBack(Item item)
         {
-            var index = BackpackItems.IndexOf(item);
-            BackpackItems[index] = null;
-            BackpackChanged?.Invoke();   
+            var index = BackPackItems.IndexOf(item);
+            BackPackItems[index] = null;
+            BackpackChanged?.Invoke();
         }
+
+        #endregion
     }
 }

f91af9750ad1ba1725c10a195c7284850f517bc8
Paul-Vasylenko <maincrafter1307@gmail.com>
2023-05-03 15:39:20 +0300

    Fix: inventory size = 15

--- Assets/Scripts/Items/Inventory.cs
+++ Assets/Scripts/Items/Inventory.cs
@@ -8,7 +8,7 @@ namespace Items
 {
     public class Inventory
     {
-        public const int InventorySize = 30;
+        public const int InventorySize = 15;
         private readonly EquipmentConditionChecker _equipmentFitter;
         private readonly Transform _player;
 

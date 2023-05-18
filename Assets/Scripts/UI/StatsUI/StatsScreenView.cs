6a677c34bfc12454bfd0bc9d2afb739a1e667a60
Vlad Zusko <vladzvu11@gmail.com>
2023-05-18 23:20:40 +0300

    GP-32 feat: add stat screen

---
+++ Assets/Scripts/UI/StatsUI/StatsScreenView.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UI.Core;
+using UI.InventoryUI.Element;
+using UI.StatsUI.Element;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+namespace UI.StatsUI
+{
+    public class StatsScreenView : ScreenView
+    {
+        [SerializeField] private Button _closeButton;
+        [SerializeField] private Transform _backPackContainer;
+
+        public List<StatSlot> StatSlots { get; private set; }
+
+        public event Action CloseClicked;
+
+        private void Awake()
+        {
+            _closeButton.onClick.AddListener(() => CloseClicked?.Invoke());
+            StatSlots = GetComponentsInChildren<StatSlot>().ToList();
+        }
+
+        private void OnDestroy()
+        {
+            _closeButton.onClick.RemoveAllListeners();
+        }
+    }
+}

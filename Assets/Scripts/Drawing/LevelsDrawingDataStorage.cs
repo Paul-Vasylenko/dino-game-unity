5f77c4bae5c16d25b0130bfa367d3a38dda6bc72
Vlad Zusko <vladzvu11@gmail.com>
2023-06-06 17:03:14 +0300

    GP-45 feat: add level drawing logic

---
+++ Assets/Scripts/Drawing/LevelsDrawingDataStorage.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Drawing.Data
+{
+    [CreateAssetMenu(fileName = nameof(LevelsDrawingDataStorage), menuName = "Drawing/LevelsDrawingDataStorage")]
+    public class LevelsDrawingDataStorage : ScriptableObject
+    {
+        [field: SerializeField] public List<LevelDrawingDataStorage> LevelsData { get; private set; }
+    }
+}
